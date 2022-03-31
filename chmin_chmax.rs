37c71030fb3ec96f3b509c07b02138f05f7a4cb9
minoru-github <kurami0621@yahoo.co.jp>
2022-03-31 23:13:30 +0900

    test: chmin,chmaxにテスト追加

--- chmin_chmax.rs
+++ chmin_chmax.rs
@@ -1,3 +1,7 @@
+#![allow(dead_code)]
+use cargo_snippet::snippet;
+
+#[snippet]
 fn chmin<T: PartialOrd>(a: &mut T, b: T) -> bool {
     if *a > b {
         *a = b;
@@ -7,6 +11,7 @@ fn chmin<T: PartialOrd>(a: &mut T, b: T) -> bool {
     }
 }
 
+#[snippet]
 fn chmax<T: PartialOrd>(a: &mut T, b: T) -> bool {
     if *a < b {
         *a = b;
@@ -15,3 +20,31 @@ fn chmax<T: PartialOrd>(a: &mut T, b: T) -> bool {
         false
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn test_chmin() {
+        let mut a = 3;
+        assert_eq!(chmin(&mut a, 1), true);
+        assert_eq!(a, 1);
+
+        let mut a = 3;
+        assert_eq!(chmin(&mut a, 5), false);
+        assert_eq!(a, 3);
+    }
+
+    #[test]
+    fn test_chmax() {
+        let mut a = 3;
+        assert_eq!(chmax(&mut a, 4), true);
+        assert_eq!(a, 4);
+
+        let mut a = 3;
+        assert_eq!(chmax(&mut a, 1), false);
+        assert_eq!(a, 3);
+    }
+}
+

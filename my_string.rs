c60d5ba0b6b3c846e48b6657ccb764e14e91c326
minoru-github <kurami0621@yahoo.co.jp>
2022-04-13 23:23:58 +0900

    feat: ProconioのChars用のreverse作成。あんまり使わないかも。

--- my_string.rs
+++ my_string.rs
@@ -8,17 +8,34 @@ fn reverse_string(S: String) -> String {
     S.chars().rev().collect()
 }
 
+#[snippet]
+fn reverse_chars(mut S: Vec<char>) -> Vec<char> {
+    //! 文字列を反転する関数
+    S.reverse();
+    S
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_reverse_string(){
+    fn test_reverse_string() {
         let S = "ABC".to_string();
         assert_eq!(reverse_string(S), "CBA");
         let S = "de2Fgg0".to_string();
         assert_eq!(reverse_string(S), "0ggF2ed");
         let S = "".to_string();
-        assert_eq!(reverse_string(S),"");
+        assert_eq!(reverse_string(S), "");
+    }
+
+    #[test]
+    fn test_reverse_chars() {
+        let S = vec!['a', 'b', 'c'];
+        assert_eq!(reverse_chars(S), ['c', 'b', 'a']);
+        let S = vec!['a', '2', 'A', ' '];
+        assert_eq!(reverse_chars(S), [' ', 'A', '2', 'a']);
+        let S = vec!['a', 'b', 'a'];
+        assert_eq!(reverse_chars(S.clone()), S);
     }
 }

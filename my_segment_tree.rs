386b70f9024fe2ff03b2bfa5ff70154d56dea11a
minoru-github <kurami0621@yahoo.co.jp>
2022-04-14 22:28:13 +0900

    feat: モノイドに最大値取得の二項演算をするMAX追加

--- my_segment_tree.rs
+++ my_segment_tree.rs
@@ -20,8 +20,20 @@ impl Monoid for SUM {
     }
 }
 
+struct MAX;
+impl Monoid for MAX {
+    type T = u64;
+    fn e() -> Self::T {
+        u64::min_value()
+    }
+    fn op(a: Self::T, b: Self::T) -> Self::T {
+        a.max(b)
+    }
+}
+
 struct SegmentTree<M: Monoid> {
     n: usize,
+    query_end: usize,
     data: Vec<M::T>,
 }
 
@@ -33,6 +45,7 @@ impl<M: Monoid> SegmentTree<M> {
         }
         SegmentTree {
             n: adjusted_n,
+            query_end: n,
             data: vec![M::e(); 2 * adjusted_n],
         }
     }
@@ -50,11 +63,23 @@ impl<M: Monoid> SegmentTree<M> {
     }
 
     pub fn get(&self, idx: usize) -> M::T {
+        if self.query_end < idx {
+            panic!("segment_tree::get() : out of bounds");
+        }
         let node_idx = idx + self.n - 1;
         self.data[node_idx].clone()
     }
 
     pub fn query(&self, left: usize, right: usize) -> M::T {
+        if self.query_end < left {
+            panic!("segment_tree::query() : left out of bounds");
+        }
+        if self.query_end < right {
+            panic!("segment_tree::query() : right out of bounds");
+        }
+        if right <= left {
+            panic!("segment_tree::query() : illegal range (right <= left)");
+        }
         self.sub_query(left, right, 0, 0, self.n)
     }
 
@@ -138,5 +163,24 @@ mod test {
                 assert_eq!(ans, expected);
             }
         }
+    }
+
+    #[test]
+    fn test_range_max() {
+        let N = 10;
+        let input = vec![1, 53, 2, 44, 102, 15, 8, 9, 0];
+        let mut st = SegmentTree::<MAX>::new(N);
+        for (idx,x) in input.iter().enumerate() {
+            st.set(idx, *x);
+        }
+
+        let ans = st.query(0, N);
+        assert_eq!(ans, 102);
+
+        let ans = st.query(5, 6);
+        assert_eq!(ans, 15);
+
+        let ans = st.query(1, 4);
+        assert_eq!(ans, 53);
     }
 }

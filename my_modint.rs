6ff4d867e082123ac3a16a59f55dfe1088feaaa0
minoru-github <kurami0621@yahoo.co.jp>
2022-04-17 22:36:44 +0900

    feat: modライブラリに"*"演算追加。

--- my_modint.rs
+++ my_modint.rs
@@ -1,11 +1,11 @@
 #![allow(non_snake_case, unused)]
 use cargo_snippet::snippet;
+use std::ops::*;
 
 struct Modint<const MODULO: usize> {
     val: usize,
 }
 
-use std::ops::Add;
 impl<const MODULO: usize> Add for Modint<MODULO> {
     type Output = Modint<MODULO>;
     fn add(self, other: Self) -> Self::Output {
@@ -14,16 +14,23 @@ impl<const MODULO: usize> Add for Modint<MODULO> {
     }
 }
 
-use std::ops::Sub;
 impl<const MODULO: usize> Sub for Modint<MODULO> {
     type Output = Modint<MODULO>;
-    fn sub(self, other: Modint<MODULO>) -> Self::Output {
+    fn sub(self, other: Self) -> Self::Output {
         let mut self_val = self.val;
         if self_val < other.val {
             self_val += MODULO;
         }
         let sub_val = (self_val - other.val) % MODULO;
         Self::Output { val: sub_val }
+    }
+}
+
+impl<const MODULO: usize> Mul for Modint<MODULO> {
+    type Output = Modint<MODULO>;
+    fn mul(self, other: Self) -> Self::Output {
+        let mul_val = (self.val * other.val) % MODULO;
+        Self::Output { val: mul_val }
     }
 }
 
@@ -93,5 +100,23 @@ mod tests {
         let y = Mint::new(10);
         let ans = x - y;
         assert_eq!(ans.val(), 999999997);
+    }
+
+    #[test]
+    fn test_mul_non_mod() {
+        type Mint = Modint<998244353>;
+        let x = Mint::new(4);
+        let y = Mint::new(17);
+        let ans = x * y;
+        assert_eq!(ans.val(), 68);
+    }
+
+    #[test]
+    fn test_mul_mod998244353() {
+        type Mint = Modint<998244353>;
+        let x = Mint::new(998244350);
+        let y = Mint::new(21738);
+        let ans = x * y;
+        assert_eq!(ans.val(), 998179139);
     }
 }

5724bb8cf4eb9d004e3bf62df5776b90330cea4e
minoru-github <kurami0621@yahoo.co.jp>
2022-10-28 00:04:58 +0900

    feat: impl Reverse trait

--- src/mylib/my_string.rs
+++ src/mylib/my_string.rs
@@ -1,18 +1,23 @@
-#![allow(non_snake_case, unused, dead_code)]
-use cargo_snippet::snippet;
+#![allow(unused, dead_code)]
 
-#[snippet]
-fn reverse_string(S: String) -> String {
-    //! 文字列を反転する関数
-    //! String型にreverseが実装されてないので自作
-    S.chars().rev().collect()
+trait Reverse {
+    fn get_reversed(&self) -> Self;
 }
 
-#[snippet]
-fn reverse_chars(mut S: Vec<char>) -> Vec<char> {
-    //! 文字列を反転する関数
-    S.reverse();
-    S
+impl Reverse for String {
+    fn get_reversed(&self) -> String {
+        //! 文字列を反転する関数
+        //! String型にreverseが実装されてないので自作
+        self.chars().rev().collect()
+    }
+}
+
+impl Reverse for Vec<char> {
+    fn get_reversed(&self) -> Vec<char> {
+        let mut s = self.clone();
+        s.reverse();
+        s
+    }
 }
 
 #[cfg(test)]
@@ -21,21 +26,21 @@ mod tests {
 
     #[test]
     fn test_reverse_string() {
-        let S = "ABC".to_string();
-        assert_eq!(reverse_string(S), "CBA");
-        let S = "de2Fgg0".to_string();
-        assert_eq!(reverse_string(S), "0ggF2ed");
-        let S = "".to_string();
-        assert_eq!(reverse_string(S), "");
+        let s = "ABC".to_string();
+        assert_eq!(s.get_reversed(), "CBA");
+        let s = "de2Fgg0".to_string();
+        assert_eq!(s.get_reversed(), "0ggF2ed");
+        let s = "".to_string();
+        assert_eq!(s.get_reversed(), "");
     }
 
     #[test]
     fn test_reverse_chars() {
-        let S = vec!['a', 'b', 'c'];
-        assert_eq!(reverse_chars(S), ['c', 'b', 'a']);
-        let S = vec!['a', '2', 'A', ' '];
-        assert_eq!(reverse_chars(S), [' ', 'A', '2', 'a']);
-        let S = vec!['a', 'b', 'a'];
-        assert_eq!(reverse_chars(S.clone()), S);
+        let mut s = vec!['a', 'b', 'c'];
+        assert_eq!(s.get_reversed(), ['c', 'b', 'a']);
+        let s = vec!['a', '2', 'A', ' '];
+        assert_eq!(s.get_reversed(), [' ', 'A', '2', 'a']);
+        let s = vec!['a', 'b', 'a'];
+        assert_eq!(s.get_reversed().get_reversed(), s);
     }
 }

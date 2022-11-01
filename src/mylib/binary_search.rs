ee8e376e13c34fe820e27bd02a26383b24268955
minoru-github <kurami0621@yahoo.co.jp>
2022-11-01 23:50:17 +0900

    refactor

--- src/mylib/binary_search.rs
+++ src/mylib/binary_search.rs
@@ -1,11 +1,17 @@
 #![allow(dead_code)]
-use cargo_snippet::snippet;
 
-#[snippet]
 fn binary_search(n: i64) -> i64 {
     //! 二分探索(めぐる式)
     let is_ok = |x: i64| -> bool {
+        // (n * n + 2 * n)以上の最小値を探す
         x >= (n * n + 2 * n)
+
+        // n以上の最小値を探す。ok は大きい数、ngは小さい数にする。
+        // x >= n
+
+        // n未満の最小値を探す。ok は小さい数、ngは大きい数にする。
+        //x < n
+
         // if 満たすべき条件 {
         //     true
         // } else {
@@ -13,8 +19,8 @@ fn binary_search(n: i64) -> i64 {
         // }
     };
 
-    let mut ok = 1000i64; // 満たす中でのでかい数
-    let mut ng = -1; // ギリギリ満たさない数
+    let mut ok = 1000i64; // 満たす中での絶対値が大きい数
+    let mut ng = -1;      // ギリギリ満たさない数
     while (ok - ng).abs() > 1 {
         let mid = (ok + ng) / 2;
         if is_ok(mid) {

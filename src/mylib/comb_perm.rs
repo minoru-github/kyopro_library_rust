68af6a7ffa8b54551fa91d797aaafe59b4c95fb1
minoru-github <kurami0621@yahoo.co.jp>
2022-11-03 12:33:06 +0900

    feat: impl comb for big number

--- src/mylib/comb_perm.rs
+++ src/mylib/comb_perm.rs
@@ -13,6 +13,16 @@ fn compute_combination_naively(n: usize, r: usize) -> usize {
     nmrt / dnmnt
 }
 
+fn compute_combination(mut n: usize, r: usize) -> usize {
+    let mut res = 1;
+    for i in 1..=r {
+        res *= n;
+        res /= i;
+        n -= 1;
+    }
+    res
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -24,9 +34,25 @@ mod test {
 
         let a = compute_combination_naively(29, 15);
         assert_eq!(a, 77558760);
-        
-        // overflow case
+
+        // overflow case in naive case
         // let a = compute_combination_naively(39, 15);
         // assert_eq!(a, 25140840660);
+    }
+
+    #[test]
+    fn test_compute_combination() {
+        let a = compute_combination(10, 2);
+        assert_eq!(a, 45);
+
+        let a = compute_combination(29, 15);
+        assert_eq!(a, 77558760);
+
+        // overflow case in naive case
+        let a = compute_combination(39, 15);
+        assert_eq!(a, 25140840660);
+
+        let a = compute_combination(60, 30);
+        assert_eq!(a, 118264581564861424);
     }
 }
